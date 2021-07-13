22d7199de19fbd189b6d8689b2c8829740839294
DamsyDeSilva <damsydesilva@gmail.com>
2021-07-13 16:29:22 +0530

    image added and title changed

--- ncms/src/pages/Home.js
+++ ncms/src/pages/Home.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import PatientRegForm from "../components/PatientRegForm";
 // import StatComponent from "../components/StatComponent";
 
-const Home = () =>  {
+const Home = () => {
   return (
     <div className="home">
       <div className="div_header">
@@ -11,16 +11,21 @@ const Home = () =>  {
       </div>
 
       <div className="div_Home_Content">
-        {/* <div className="div_stats">
-          <StatComponent/>
-        </div> */}
+        <div className="div_image">
+
+          <img
+            src={
+              "https://media.istockphoto.com/vectors/group-of-people-wearing-medical-masks-vector-id1202623453?k=6&m=1202623453&s=170667a&w=0&h=SfNIc7aXyRGfokRnYDvFDZy2DE6XNDqXZqPKO5thP7s="
+            }
+            alt="Logo"
+          />
+        </div>
 
         <div className="div_patient_reg_form">
           <PatientRegForm />
         </div>
-
       </div>
     </div>
   );
-}
+};
 export default Home;
