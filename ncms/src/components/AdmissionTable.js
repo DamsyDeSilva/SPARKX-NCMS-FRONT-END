f9ed89270ed3839a6b29f6ac8cff4e22eb0c4dba
DamsyDeSilva <damsydesilva@gmail.com>
2021-07-13 08:36:51 +0530

    patient admission ui implemented

---
+++ ncms/src/components/AdmissionTable.js
@@ -0,0 +1,174 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Avatar,
+  Grid,
+  Typography,
+  TablePagination,
+  TableFooter,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 650,
+  },
+  tableContainer: {
+    borderRadius: 15,
+    margin: "10px 10px",
+    maxWidth: 950,
+  },
+  tableHeaderCell: {
+    fontWeight: "bold",
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.getContrastText(theme.palette.primary.dark),
+  },
+  avatar: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.getContrastText(theme.palette.primary.light),
+  },
+  name: {
+    fontWeight: "bold",
+    color: theme.palette.secondary.dark,
+  },
+  status: {
+    fontWeight: "bold",
+    fontSize: "0.75rem",
+    color: "white",
+    backgroundColor: "grey",
+    borderRadius: 8,
+    padding: "3px 10px",
+    display: "inline-block",
+  },
+  title: {
+    fontSize: "20px",
+    color: "#f1356d",
+    marginBottom: "30px",
+  },
+}));
+
+const AdmissionTable = () => {
+  const [patientAdmissionData, setPatientAdmissionData] = useState([]);
+
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  useEffect(() => {
+    const config = {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    };
+
+    axios
+      .get("http://localhost:8080/ncms/doctor/admits", config)
+      .then((response) => {
+        // hospitalData = response.data;
+        console.log(response.data);
+        setPatientAdmissionData(response.data);
+      })
+      // console.log(hospitalData);
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <TableContainer component={Paper} className={classes.tableContainer}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableHeaderCell}>
+                Patient Id and Name
+              </TableCell>
+              <TableCell className={classes.tableHeaderCell}>
+                District
+              </TableCell>
+              <TableCell className={classes.tableHeaderCell}>
+                Location
+              </TableCell>
+              <TableCell className={classes.tableHeaderCell}>Gender</TableCell>
+              <TableCell className={classes.tableHeaderCell}>
+                HospitalID
+              </TableCell>
+              <TableCell className={classes.tableHeaderCell}>
+                Bed Number
+              </TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {patientAdmissionData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell>
+                    <Grid container>
+                      <Grid item lg={2}>
+                        <Avatar
+                          alt={row.name}
+                          src="."
+                          className={classes.avatar}
+                        />
+                      </Grid>
+
+                      <Grid item lg={10}>
+                        <Typography className={classes.name}>
+                          {row.firstName} {row.lastName}
+                        </Typography>
+                        <Typography color="textSecondary" variant="body2">{row.id}</Typography>
+                      </Grid>
+                    </Grid>
+                  </TableCell>
+
+                  <TableCell>
+                    <Typography color="primary" variant="subtitle2">
+                      {row.district}
+                    </Typography>
+                  </TableCell>
+
+                  <TableCell>
+                    {row.locationX}
+                    {" , "}
+                    {row.locationY}
+                  </TableCell>
+
+                  <TableCell>{row.gender}</TableCell>
+                  <TableCell>{row.hospitalId}</TableCell>
+                  <TableCell>{row.bedNo}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+          <TableFooter>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 15]}
+              component="div"
+              count={patientAdmissionData.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+export default AdmissionTable;
--- ncms/src/pages/PatientAdmission.js
+++ ncms/src/pages/PatientAdmission.js
@@ -1,15 +1,33 @@
-// import "./Home.css";
 import "./pageStyles.css";
 import Header from "../components/Header";
+import AdmissionTable from "../components/AdmissionTable";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    fontSize: "20px",
+    color: "#f1356d",
+    marginBottom: "30px",
+  },
+}));
+
 
 const PatientAdmission = () =>  {
+  const classes = useStyles();
   return (
     <div className="home">
       <div className="div_header">
         <Header />
       </div>
 
-      <p>Patient Admission</p>
+      <h1 className={classes.title}> Patient Admission</h1>
+      <div className="div_Home_Content">
+
+        <div >
+          <AdmissionTable />
+        </div>
+
+      </div>
     </div>
   );
 }
